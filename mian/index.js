884987311b743b023b64fa67bd31b56e7065babe
hieu16-1-04 <phieu1601@gmail.com>
2025-04-25 23:49:18 +0700

    Cập nhật giao diện phong bì thư

---
+++ mian/index.js
@@ -0,0 +1,5 @@
+function openLetter() {
+    document.getElementById("letter").classList.add("open"); // Mở thư
+    document.getElementById("envelope").classList.add("opened"); // Đóng phong bì
+  }
+  
