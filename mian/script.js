73278931156a1a54b390c51858d8ab31091d5364
hieu16-1-04 <phieu1601@gmail.com>
2025-04-26 00:04:36 +0700

    Thông điệp commit

--- mian/script.js
+++
@@ -1,5 +0,0 @@
-function openLetter() {
-    document.getElementById("letter").classList.add("open"); // Mở thư
-    document.getElementById("envelope").classList.add("opened"); // Đóng phong bì
-  }
-  
